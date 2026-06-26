84251ef84240057d41751f409bdac5798d091be9
bhaleraorushi1213 <bhaleraorushi1213@gmail.com>
2026-06-26 18:50:06 +0530

    created basic structure of database models

---
+++ server/src/models/document.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema(
+  {
+    fileName: {
+      type: String,
+      required: true
+    },
+    fileUrl: {
+      type: String,
+      required: true
+    },
+    fileSize: {
+      type: Number,
+      required: true
+    },
+    fileType: {
+      type: String,
+      required: true
+    },
+    folder: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Folder",
+    },
+    uploadedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    approvalStatus: {
+      type: String,
+      enum: ["Pending", "Approved", "Rejected"],
+      default: "Pending",
+    },
+  }
+);
+
+const Document = mongoose.model("Document", documentSchema);
+export default documentSchema;
---
+++ server/src/models/share.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const shareSchema = new mongoose.Schema(
+  {
+    document: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Document",
+      required: true
+    },
+
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    sharedWith: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    permission: {
+      type: String,
+      enum: ["Read", "Write", "Download"],
+      default: "Read",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Share = mongoose.model("Share", shareSchema);
+export default Share;
